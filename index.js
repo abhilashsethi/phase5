dc8d9a70a536e02df59c945b586263710928cbd6
abhilash <abhilashsethi9@gmail.com>
2026-08-15 10:03:04 +0530

    second commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/health", (req, res) => {
 })
 
 app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello from phase5!" });
+  return res.status(200).json({ message: "Hello from phase5 its Abhilash!" });
 })
 
 app.listen(port, () => {
